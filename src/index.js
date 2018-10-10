3a6bd2645f410f57fa51048664b3e12e0137024c
Alex Andrade <g.alex.andrade@gmail.com>
2018-10-10 08:11:00 -0300

    Middleware

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
@@ -14,7 +14,20 @@ const rootReducer = combineReducers({
     res: resultReducer
 });
 
-const store = createStore(rootReducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState());
+
+            return result;
+        }
+    };
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();

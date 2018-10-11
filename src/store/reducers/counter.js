de62c0a407c7aa64bbcf54f173cdf438fc978bfd
Alex Andrade <g.alex.andrade@gmail.com>
2018-10-11 17:22:33 -0300

    Leaner switch

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -12,12 +12,6 @@ const reducer = (state = initialState, action) => {
         case actionTypes.DECREMENT:
             return updateObject(state, {counter: state.counter - 1});
         case actionTypes.ADD:
-            /* It remains here just as a sample
-            return {
-                ...state,
-                counter: state.counter + action.payload
-            }
-            */
             return updateObject(state, {counter: state.counter + action.payload});
         case actionTypes.SUBTRACT:
             return updateObject(state, {counter: state.counter - action.payload});
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -4,33 +4,41 @@ const initialState = {
     results: []
 }
 
+const storeResult = (state, action) => {
+    return {
+        ...state,
+        //CONCAT returns a NEW array. PUSH touches the original array, witch is not allowed here
+        results: state.results.concat({
+            id: new Date(),
+            value: action.result
+        })
+    }
+}
+
+const deleteResult = (state, action) => {
+    /* WAY OF UPDATE THE ARRAY IMMUTABLY
+    const fakeIdx = 1;
+    //Create a coppy of the array NOT DEEPLY
+    const newArray = [...state.results]
+    newArray.splice(fakeIdx, 1);
+    */
+
+    //Return a new array
+    const newArray = state.results.filter(el => el.id !== action.id );
+
+    return {
+        ...state,
+        //CONCAT returns a NEW array. PUSH touches the original array, witch is not allowed here
+        results: newArray
+    }
+}
+
 const reducer = (state = initialState, action) => {
     switch(action.type){
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                //CONCAT returns a NEW array. PUSH touches the original array, witch is not allowed here
-                results: state.results.concat({
-                    id: new Date(),
-                    value: action.result
-                })
-            }
+            return storeResult(state, action);
         case actionTypes.DELETE_RESULT:
-            /* WAY OF UPDATE THE ARRAY IMMUTABLY
-            const fakeIdx = 1;
-            //Create a coppy of the array NOT DEEPLY
-            const newArray = [...state.results]
-            newArray.splice(fakeIdx, 1);
-            */
-
-            //Return a new array
-            const newArray = state.results.filter(el => el.id !== action.id );
-
-            return {
-                ...state,
-                //CONCAT returns a NEW array. PUSH touches the original array, witch is not allowed here
-                results: newArray
-            }
+            return deleteResult(state, action);
     }
 
     return state;
